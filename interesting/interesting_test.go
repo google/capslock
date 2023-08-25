e9b8cef9eb89f270cca9ac0020f09aa07e388a32
Damien Miller <djm@google.com>
2023-08-25 11:48:47 +1000

    Infrastructure and tests for loading user maps
    This is the interesting package infrastructure to load user capability map
    files and optionally merge them with the default builtin one. Includes some
    basic tests.

--- interesting/interesting_test.go
+++ interesting/interesting_test.go
@@ -7,10 +7,25 @@
 package interesting
 
 import (
+	"strings"
 	"testing"
 
 	cpb "github.com/google/capslock/proto"
 )
+
+const (
+	userCapabilityMap = `
+# Override existing package capability
+package runtime CAPABILITY_NETWORK
+# Specify package capability for a new package
+package example.com/some/package CAPABILITY_OPERATING_SYSTEM
+# Specify package capability for a new function
+func example.com/some/package.Foo CAPABILITY_FILES
+# Override existing function capability
+func fmt.Sprintf CAPABILITY_FILES
+`
+)
+
 
 func TestInteresting(t *testing.T) {
 	classifier := DefaultClassifier()
@@ -34,6 +49,11 @@ func TestInteresting(t *testing.T) {
 			cpb.Capability_CAPABILITY_UNSPECIFIED,
 		},
 		{
+			"example.com/some/package",
+			"example.com/some/package.Foo_Cfunc_GoString",
+			cpb.Capability_CAPABILITY_CGO,
+		},
+		{
 			"os",
 			"os.SomeNewFunctionWithNoFunctionLevelCategoryYet",
 			cpb.Capability_CAPABILITY_OPERATING_SYSTEM,
@@ -47,6 +67,108 @@ func TestInteresting(t *testing.T) {
 			"runtime",
 			"(*runtime.Func).Name",
 			cpb.Capability_CAPABILITY_SAFE,
+		},
+		{
+			"foo",
+			"foo.Something_Cfunc_GoString",
+			cpb.Capability_CAPABILITY_CGO,
+		},
+		{
+			"foo",
+			"foo.Something",
+			cpb.Capability_CAPABILITY_UNSPECIFIED,
+		},
+	} {
+		if got := classifier.FunctionCategory(c.pkg, c.fn); got != c.want {
+			t.Errorf("FunctionCategory(%q, %q): got %q, want %q", c.pkg, c.fn, got, c.want)
+		}
+	}
+}
+
+func TestUserWithBuiltin(t *testing.T) {
+	classifier, err := LoadClassifier(t.Name(), strings.NewReader(userCapabilityMap), false)
+	if err != nil {
+		t.Fatalf("LoadClassifier failed: %v", err)
+	}
+	for _, c := range []struct {
+		pkg, fn string
+		want    cpb.Capability
+	}{
+		{
+			"os",
+			"os.Open",
+			cpb.Capability_CAPABILITY_FILES,
+		},
+		{
+			"os",
+			"os.SomeNewFunctionWithNoFunctionLevelCategoryYet",
+			cpb.Capability_CAPABILITY_OPERATING_SYSTEM,
+		},
+		{
+			"fmt",
+			"fmt.Sprintf",
+			cpb.Capability_CAPABILITY_FILES,
+		},
+		{
+			"example.com/some/package",
+			"example.com/some/package.Foo",
+			cpb.Capability_CAPABILITY_FILES,
+		},
+		{
+			"example.com/some/package",
+			"example.com/some/package.OtherFoo",
+			cpb.Capability_CAPABILITY_OPERATING_SYSTEM,
+		},
+		{
+			"runtime",
+			"runtime.SomeFunction",
+			cpb.Capability_CAPABILITY_NETWORK,
+		},
+	} {
+		if got := classifier.FunctionCategory(c.pkg, c.fn); got != c.want {
+			t.Errorf("FunctionCategory(%q, %q): got %q, want %q", c.pkg, c.fn, got, c.want)
+		}
+	}
+}
+
+func TestUserWithoutBuiltin(t *testing.T) {
+	classifier, err := LoadClassifier(t.Name(), strings.NewReader(userCapabilityMap), true)
+	if err != nil {
+		t.Fatalf("LoadClassifier failed: %v", err)
+	}
+	for _, c := range []struct {
+		pkg, fn string
+		want    cpb.Capability
+	}{
+		{
+			"os",
+			"os.Open",
+			cpb.Capability_CAPABILITY_UNSPECIFIED,
+		},
+		{
+			"os",
+			"os.SomeNewFunctionWithNoFunctionLevelCategoryYet",
+			cpb.Capability_CAPABILITY_UNSPECIFIED,
+		},
+		{
+			"fmt",
+			"fmt.Sprintf",
+			cpb.Capability_CAPABILITY_FILES,
+		},
+		{
+			"example.com/some/package",
+			"example.com/some/package.Foo",
+			cpb.Capability_CAPABILITY_FILES,
+		},
+		{
+			"example.com/some/package",
+			"example.com/some/package.OtherFoo",
+			cpb.Capability_CAPABILITY_OPERATING_SYSTEM,
+		},
+		{
+			"runtime",
+			"runtime.SomeFunction",
+			cpb.Capability_CAPABILITY_NETWORK,
 		},
 	} {
 		if got := classifier.FunctionCategory(c.pkg, c.fn); got != c.want {
